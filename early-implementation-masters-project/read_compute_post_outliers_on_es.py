c5240551e3e69fd24a243a3da42e6ff2cc8696df
Swetha Chandrasekar <swe.chand@gmail.com or swetha.chandrasekar@sjsu.edu>
2019-04-24 18:04:47 -0700

    Checking in

--- early-implementation-masters-project/read_compute_post_outliers_on_es.py
+++ early-implementation-masters-project/read_compute_post_outliers_on_es.py
@@ -1,7 +1,10 @@
+import math
+import certifi
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 import sys
+import elasticsearch
 from elasticsearch import Elasticsearch
 import json
 import os
@@ -20,6 +23,35 @@ import logging
 def search(es_object, index_name, document,search):
     res = es_object.search(index=index_name,  body=search)
     return res
+def populateIndexes(es, index_name, docType):
+  jsonstr= '{ "@timestamp":"2019-03-01T08:05:34.853Z", "event":{ "dataset":"prometheus.collector", "duration":115000, "module":"prometheus" }, "metricset":{ "name":"collector" }, "prometheus":{ "labels":{ "listener_name":"http" }, "metrics":{ "net_conntrack_listener_conn_accepted_total":3, "net_conntrack_listener_conn_closed_total":0 } }, "service":{ "address":"127.0.0.1:55555", "type":"prometheus" } }'
+  i = 0
+  counter = 0
+  for i in range(0,60):
+    for j in range(0,24):
+      for k in range(1,15):
+        to_replace = "2019-03-01T08:05:34.853Z"
+        kstr = str(k)
+        if (k < 10):
+          kstr = "0" + kstr#+ str(k)
+        istr = str(i)
+        if (i < 10):
+          istr = "0" + istr
+        jstr = str(j)
+        if (j < 10):
+          jstr = "0" + jstr
+
+        replace_with = "2019-03-"+kstr+"T" + jstr+ ":"+ istr+":34.853Z"
+        targetTimeStamp = jsonstr.replace(to_replace, replace_with)
+        to_replace_metric = '"net_conntrack_listener_conn_accepted_total":3'
+        conn = math.sin(counter)*10 + 20
+        counter+=1
+        if (conn < 0):
+          conn *= -1
+        conn = int(conn)
+        replace_metric_with = '"net_conntrack_listener_conn_accepted_total":' + str(conn)
+        targetJson = targetTimeStamp.replace(to_replace_metric, replace_metric_with)
+        es.index(index=index_name, doc_type="metric", body=json.loads(targetJson))
 
 def connect_elasticsearch():
     _es = None
@@ -32,7 +64,9 @@ def connect_elasticsearch():
 def compute_outliers(metrics, metric_key, test_samples, sort_key_list, es):
   train = metrics[metric_key][:-test_samples]
   test = metrics[metric_key][-test_samples:]
-  
+#print ("length of train:" + str(len(train))) 
+# print ("length of metrics:" + str(len(metrics[metric_key]))) 
+# print ("length of test:" + str(len(test))) 
   test_time_stamps = sort_key_list[-test_samples:]
   
   X_train = pd.DataFrame(train, columns = ['sample'])
@@ -48,7 +82,11 @@ def compute_outliers(metrics, metric_key, test_samples, sort_key_list, es):
     es.index(index=anomalies_index, doc_type='predicted', body=json.loads(jsonString))
 
 if __name__ == '__main__':
-  es = connect_elasticsearch()
+  #es = connect_elasticsearch()
+  es = elasticsearch.Elasticsearch(['https://e88c64e7d90e4ecaba68d3830ac8ba85.us-central1.gcp.cloud.es.io:9243/'], http_auth=('elastic', '5qOa6DEOxDHepgGUSYvM3zpT'),use_ssl=True, ca_certs=certifi.where())
+#if es.ping():
+#   print ("Successfully connected to Elastic Search")
+#   populateIndexes(es, 'metrics', '')
   logging.basicConfig(level=logging.ERROR)
 
   index_name = sys.argv[1] ## name of the index from which the metric documents are read
@@ -62,7 +100,7 @@ if __name__ == '__main__':
   
   if es is not None:
  
-    search_object = '{"sort" : [{"' + str(sort_key) + '" : {"order" : "asc"}}]}'
+    search_object = '{"sort" : [{"' + str(sort_key) + '" : {"order" : "asc"}}],"from" : 0, "size" : 10000}'
     
     response= es.search(index=str(index_name),body=search_object)
     metrics = {}
@@ -83,11 +121,11 @@ if __name__ == '__main__':
 
     for metric_key in metrics:
       print ("computing outliers for " + metric_key)
-      input()
+      print (len(metrics[metric_key]))
       if (len(metrics[metric_key]) != len(sort_key_list)):
         raise "time stamps collected != metric sample list"
 
-
+      print ("Test samples:"+str(test_samples)) 
       compute_outliers(metrics, metric_key, test_samples, sort_key_list, es)
 
 
